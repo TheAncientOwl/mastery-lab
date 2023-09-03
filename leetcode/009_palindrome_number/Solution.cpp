6b99777a19f50e455134fc7f1e54f5e2a78b4046
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-09-04 01:21:34 +0300

    leetcode 009

---
+++ leetcode/009_palindrome_number/Solution.cpp
@@ -0,0 +1,86 @@
+#include <gtest/gtest.h>
+
+class Solution
+{
+public:
+    bool isPalindrome(int x) const
+    {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+
+        int fast{ x };
+        int slow{ x };
+        int reversed_half{ 0 };
+
+        while (fast)
+        {
+            reversed_half = reversed_half * 10 + slow % 10;
+            slow /= 10;
+            fast /= 100;
+        }
+
+        return slow == reversed_half || slow == reversed_half / 10;
+    }
+};
+
+TEST(Leetcode_009_palindrome_number, test01)
+{
+    const int in = 121;
+    const bool expected = true;
+    const bool out = Solution{}.isPalindrome(in);
+
+    EXPECT_EQ(out, expected);
+}
+
+TEST(Leetcode_009_palindrome_number, test02)
+{
+    const int in = -121;
+    const bool expected = false;
+    const bool out = Solution{}.isPalindrome(in);
+
+    EXPECT_EQ(out, expected);
+}
+
+TEST(Leetcode_009_palindrome_number, test03)
+{
+    const int in = 10;
+    const bool expected = false;
+    const bool out = Solution{}.isPalindrome(in);
+
+    EXPECT_EQ(out, expected);
+}
+
+TEST(Leetcode_009_palindrome_number, test04)
+{
+    const int in = 1111;
+    const bool expected = true;
+    const bool out = Solution{}.isPalindrome(in);
+
+    EXPECT_EQ(out, expected);
+}
+
+TEST(Leetcode_009_palindrome_number, test05)
+{
+    const int in = 1231;
+    const bool expected = false;
+    const bool out = Solution{}.isPalindrome(in);
+
+    EXPECT_EQ(out, expected);
+}
+
+TEST(Leetcode_009_palindrome_number, test06)
+{
+    const int in = 21120;
+    const bool expected = false;
+    const bool out = Solution{}.isPalindrome(in);
+
+    EXPECT_EQ(out, expected);
+}
+
+TEST(Leetcode_009_palindrome_number, test07)
+{
+    const int in = 0;
+    const bool expected = true;
+    const bool out = Solution{}.isPalindrome(in);
+
+    EXPECT_EQ(out, expected);
+}
---
+++ scripts/leetcode.sh
@@ -0,0 +1,40 @@
+if [ ! $# -eq 1  ]; then
+    echo ">> Missing leetcode problem name"
+    exit 1
+fi
+
+problem_name=$1
+problem_path="./leetcode/$1"
+
+# Make dir + add to CMakeLists.txt
+mkdir -p $problem_path
+grep -qxF "add_subdirectory($problem_name)" ./leetcode/CMakeLists.txt || echo "add_subdirectory($problem_name)" >> ./leetcode/CMakeLists.txt
+
+# CMakeLists.txt
+cd $problem_path
+echo "file(GLOB SOURCE_FILES "*.cpp")
+add_executable(Leetcode_${problem_name} \${SOURCE_FILES})
+
+target_link_libraries(Leetcode_${problem_name} PRIVATE
+    gtest
+    gtest_main
+)
+
+include(GoogleTest)
+gtest_discover_tests(Leetcode_${problem_name})" > CMakeLists.txt
+
+# Solution.cpp
+echo "#include <gtest/gtest.h>
+
+TEST(Leetcode_${problem_name}, test01)
+{
+
+}
+
+" > Solution.cpp
+
+# README.md
+touch README.md
+
+# Go back
+cd ../..
