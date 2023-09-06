6b51bd2a88acff2943f4ed99d44817e9b2d79d01
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-09-07 00:25:03 +0300

    improved script

--- leetcode/scripts/setup_problem.sh
+++ leetcode/scripts/setup_problem.sh
@@ -1,44 +1,60 @@
-if [ ! $# -eq 1  ]; then
-    echo ">> Missing leetcode problem name"
+# /bin/bash
+if [ ! $# -eq 2  ]; then
+    echo ">> Missing leetcode problem number nor problem url"
+    echo ">> Usage: ./setup_problem 1 https://leetcode.com/problems/two-sum/"
     exit 1
 fi
 
-problem_name=$1
-problem_path="./leetcode/$1"
+problem_number=$(printf "%04d" "$1")
+problem_url=$2
+
+raw_problem_name=$(echo "$problem_url" | grep -oP "https://leetcode.com/problems/\K[^/]+")
+if [ -z "$raw_problem_name" ]; then
+    echo "Invalid problem name '$raw_problem_name'"
+fi
+
+raw_problem_name=$(echo "$raw_problem_name" | tr '[:upper:]' '[:lower:]')
+raw_problem_name=$(echo "$raw_problem_name" | tr '-' '_')
+
+problem_name=$(echo "$raw_problem_name" | tr '_' ' ')
+problem_title=$(echo "$problem_name" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1')
+problem_id="${problem_number}_${raw_problem_name}"
+problem_path="./leetcode/$problem_id"
 
 # Make dir + add to CMakeLists.txt
 mkdir -p $problem_path
-grep -qxF "add_subdirectory($problem_name)" ./leetcode/CMakeLists.txt || echo "add_subdirectory($problem_name)" >> ./leetcode/CMakeLists.txt
+grep -qxF "add_subdirectory($problem_id)" ./leetcode/CMakeLists.txt || echo "add_subdirectory($problem_id)" >> ./leetcode/CMakeLists.txt
 
 # CMakeLists.txt
 cd $problem_path
 echo "file(GLOB SOURCE_FILES "*.cpp")
-add_executable(Leetcode_${problem_name} \${SOURCE_FILES})
+add_executable(Leetcode_${problem_id} \${SOURCE_FILES})
 
-target_link_libraries(Leetcode_${problem_name} PRIVATE
+target_link_libraries(Leetcode_${problem_id} PRIVATE
     gtest
     gtest_main
 )
 
-target_include_directories(Leetcode_${problem_name} PRIVATE \${CMAKE_CURRENT_SOURCE_DIR}/../common)
+target_include_directories(Leetcode_${problem_id} PRIVATE \${CMAKE_CURRENT_SOURCE_DIR}/../common)
 
 include(GoogleTest)
-gtest_discover_tests(Leetcode_${problem_name})" > CMakeLists.txt
+gtest_discover_tests(Leetcode_${problem_id})" > CMakeLists.txt
 
 # Solution.cpp
 echo "#include <gtest/gtest.h>
 
-TEST(Leetcode_${problem_name}, test01)
+
+
+TEST(Leetcode_${problem_id}, test01)
 {
 
-}
-" > Solution.cpp
+}" > Solution.cpp
 
 # README.md
-echo "## {Title}
-#### {problem link}
+echo "## ${problem_number}. ${problem_title}
+#### ${problem_url}
 
-{Content}
+
 
 ### Example 1:
 \`\`\`
