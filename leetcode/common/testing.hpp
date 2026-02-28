6e7ddbcd387864eb5c495924f81fa6c799086e8b
TheAncientOwl <dele.alex.o.o@gmail.com>
2026-02-28 23:32:28 +0200

    add << chaining support for expect_eq macro

--- leetcode/common/testing.hpp
+++ leetcode/common/testing.hpp
@@ -9,19 +9,33 @@
 #define GREEN "\033[32m"
 #define RESET "\033[0m"
 
-#define EXPECT_EQ(actual, expected)                                                                            \
-    do                                                                                                         \
-    {                                                                                                          \
-        auto _actual = (actual);                                                                               \
-        auto _expected = (expected);                                                                           \
-        if (!(_actual == _expected))                                                                           \
-        {                                                                                                      \
-            std::cerr << RED << "EXPECT_EQ failed at " << __FILE__ << ":" << __LINE__ << RESET << std::endl;   \
-            std::cerr << "  Actual:   " << _actual << std::endl;                                               \
-            std::cerr << "  Expected: " << _expected << std::endl;                                             \
-        }                                                                                                      \
-        else                                                                                                   \
-        {                                                                                                      \
-            std::cerr << GREEN << "EXPECT_EQ passed at " << __FILE__ << ":" << __LINE__ << RESET << std::endl; \
-        }                                                                                                      \
-    } while (0)
+struct ExpectEqStream
+{
+    bool failed;
+
+    explicit ExpectEqStream(bool f) : failed(f) {}
+
+    template <typename T>
+    ExpectEqStream &operator<<(const T &value)
+    {
+        if (failed)
+        {
+            std::cout << value;
+        }
+        return *this;
+    }
+};
+
+#define EXPECT_EQ(actual, expected) \
+    ([&]() -> ExpectEqStream {                                                                                      \
+        auto _actual = (actual);                                                                                     \
+        auto _expected = (expected);                                                                                 \
+        if (!(_actual == _expected)) {                                                                               \
+            std::cout << RED << "EXPECT_EQ failed at " << __FILE__ << ":" << __LINE__ << RESET << std::endl;     \
+            std::cout << "  Actual:   " << _actual << std::endl;                                                   \
+            std::cout << "  Expected: " << _expected << std::endl;                                                 \
+            return ExpectEqStream(true);                                                                             \
+        } else {                                                                                                     \
+            std::cout << GREEN << "EXPECT_EQ passed at " << __FILE__ << ":" << __LINE__ << RESET << std::endl;   \
+            return ExpectEqStream(false);                                                                            \
+        } })()
